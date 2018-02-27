c399f7ab2e89819db3421feb07185921ea0797f6
Reginaldo da silva souza <covinha_reginaldo@hotmail.com>
2018-02-26 20:42:14 -0400

    alteraçoes dia 26/02/2018

--- App/DAO/Conexao.php
+++ App/DAO/Conexao.php
@@ -11,5 +11,20 @@ namespace App\DAO;
 
 class Conexao
 {
+ protected $conexao;
+ private $datebase = "db_estoque";
+ private $host = "localhost";
+ private $user = "root";
+ private  $senha = "#9258863709202202#";
+
+    /**
+     * Conexao constructor.
+     */
+    public function __construct()
+    {
+        $this->conexao = new \PDO("mysql:dbname={$this->datebase};host={$this->host}", "$this->user", "$this->senha");
+        $this->conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
 
 }
